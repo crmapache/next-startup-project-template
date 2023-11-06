c5cb9891905ff975867086350506cb0bafbc4e3e
Maksim Zolotoi <crmapache@icloud.com>
2023-11-06 14:54:02 -0600

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,6 @@
-import { GeneralLayout } from '@features'
 import { MainProvider } from '@providers'
 import { RootLayoutProps } from '@types'
 
 export default function RootLayout({ children }: RootLayoutProps) {
-  return (
-    <MainProvider>
-      <GeneralLayout>{children}</GeneralLayout>
-    </MainProvider>
-  )
+  return <MainProvider>{children}</MainProvider>
 }
